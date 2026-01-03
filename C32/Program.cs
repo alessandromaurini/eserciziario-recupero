d3630e773ac1f853027d445b92d758a2d0eef52d
Alesandro Maurini <maurini.alessandro.23@itisriva.edu.it>
2026-01-03 16:09:01 +0100

    Add files via upload

---
+++ C32/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C32
+{
+    internal class Program
+    {
+        // Crea un ciclo che legge interi positivi e stampa il massimo trovato finché non viene inserito un numero negativo.
+        static void Main(string[] args)
+        {
+            int n;
+            int nmax = 0;
+            Console.WriteLine("Scrivi un numero");
+            do
+            {
+                n = int.Parse(Console.ReadLine());
+                if(n > nmax)
+                    nmax = n;
+            }
+            while (n > 0);
+            Console.WriteLine($"il numero piu grande e' {nmax}");
+        }
+    }
+}
