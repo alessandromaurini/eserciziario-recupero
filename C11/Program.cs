2328d30f6f3f710010d67e8e99d9f707c67872ba
Alesandro Maurini <maurini.alessandro.23@itisriva.edu.it>
2026-01-03 15:27:02 +0100

    Add files via upload

---
+++ C11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C11
+{
+    internal class Program
+    {
+        // Scrivi un algoritmo che conta quanti multipli di 7 ci sono tra 1 e 100.
+        static void Main(string[] args)
+        {
+            int n = 0;
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 7 == 0)
+                { 
+                    n++;
+                }
+            }
+            Console.WriteLine($"Tra 1 e 100 ci sono {n} multipli di 7");
+        }
+    }
+}
